b78b96d32ae84387eea77bbd1dfbb395066363a2
hideto-fukushima <hide1998taka723@gmail.com>
2023-11-28 14:45:13 +0900

    ６章課題提

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,6 +1,6 @@
 $(window).on(`load`,function(){
   console.log(`loadイベントが発生しました`);
 });
-$(document).scroll(function(){
+$(document).on(`scroll`,function(){
   console.log(`scrollイベントが発生しました`);
 });
